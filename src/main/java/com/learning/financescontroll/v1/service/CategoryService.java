4fda4bc3ce6023a71494693952a12fbaf2495816
Deivid <deividgabrielproc@gmail.com>
2021-05-11 14:28:41 -0300

    update: Added CategoryException handling on CategoryService

--- src/main/java/com/learning/financescontroll/v1/service/CategoryService.java
+++ src/main/java/com/learning/financescontroll/v1/service/CategoryService.java
@@ -8,18 +8,19 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.learning.financescontroll.entity.CategoryEntity;
 import com.learning.financescontroll.repository.ICategoryRepository;
 import com.learning.financescontroll.v1.dto.CategoryDto;
-
-import lombok.extern.slf4j.Slf4j;
+import com.learning.financescontroll.v1.exception.CategoryException;
 
 @CacheConfig(cacheNames = "categoria")
-@Slf4j
 @Service
 public class CategoryService implements ICategoryService {
+	
+	private String ERRO_GENERICO = "Erro interno identificado, Contate o suporte";
 	
 	private ICategoryRepository categoryRepository;
 	private ModelMapper mapper;
@@ -35,12 +36,14 @@ public class CategoryService implements ICategoryService {
 	public List<CategoryDto> listar() {
 		try {
 			return this.mapper.map(this.categoryRepository.findAll(), new TypeToken<List<CategoryDto>>() {}.getType());
+		} catch (CategoryException c) {
+			throw c;
 		} catch (Exception e) {
-			log.error("Exception happened on listar: " + e.getCause().getMessage());
-			throw e;
+			throw new CategoryException(ERRO_GENERICO, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
+	@CachePut(key = "#id")
 	@Override
 	public CategoryDto consultar(Long id) {
 		try {
@@ -48,10 +51,11 @@ public class CategoryService implements ICategoryService {
 			if (categoryOptional.isPresent()) {
 				return this.mapper.map(categoryOptional.get(), CategoryDto.class);
 			}
-			return null;
+			throw new CategoryException("Categoria não encontrada", HttpStatus.NOT_FOUND);
+		} catch (CategoryException c) {
+			throw c;
 		} catch (Exception e) {
-			log.error("Exception happened on consultar: " + e.getCause().getMessage());
-			return null;
+			throw new CategoryException(ERRO_GENERICO, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -59,15 +63,16 @@ public class CategoryService implements ICategoryService {
 	public Boolean cadastrar(CategoryDto category) {
 		try {
 			if (category.getId() != null) {
-				return Boolean.FALSE;
+				throw new CategoryException("Não pode existir ID em cadastrar.", HttpStatus.BAD_REQUEST);
 			}
 			
 			CategoryEntity categoryEntity = this.mapper.map(category, CategoryEntity.class);
 			this.categoryRepository.save(categoryEntity);
 			return Boolean.TRUE;
+		} catch (CategoryException c) {
+			throw c;
 		} catch (Exception e) {
-			log.error("Exception happened on cadastrar: " + e.getCause().getMessage());
-			return null;
+			throw new CategoryException(ERRO_GENERICO, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -83,9 +88,10 @@ public class CategoryService implements ICategoryService {
 			CategoryEntity categoryEntity = this.mapper.map(category, CategoryEntity.class);
 			this.categoryRepository.save(categoryEntity);
 			return Boolean.TRUE;
+		} catch (CategoryException c) {
+			throw c;
 		} catch (Exception e) {
-			log.error("Exception happened on atualizar: " + e.getCause().getMessage());
-			return Boolean.FALSE;
+			throw new CategoryException(ERRO_GENERICO, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -95,9 +101,10 @@ public class CategoryService implements ICategoryService {
 			this.consultar(id);
 			this.categoryRepository.deleteById(id);
 			return Boolean.TRUE;
+		} catch (CategoryException c) {
+			throw c;
 		} catch (Exception e) {
-			log.error("Exception happened on excluir: " + e.getCause().getMessage());
-			return Boolean.FALSE;
+			throw new CategoryException(ERRO_GENERICO, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
