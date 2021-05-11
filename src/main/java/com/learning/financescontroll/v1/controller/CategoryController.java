ba895e2ef117071e9f12cc4ac5f479c1c31da304
Deivid <deividgabrielproc@gmail.com>
2021-05-11 17:11:17 -0300

    update: Rest Controller and Service done for Category

--- src/main/java/com/learning/financescontroll/v1/controller/CategoryController.java
+++ src/main/java/com/learning/financescontroll/v1/controller/CategoryController.java
@@ -1,10 +1,103 @@
 package com.learning.financescontroll.v1.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.financescontroll.v1.dto.CategoryDto;
+import com.learning.financescontroll.v1.model.ResponseModel;
+import com.learning.financescontroll.v1.service.ICategoryService;
 
 @RestController
 @RequestMapping("/v1/categoria")
 public class CategoryController {
-	
+
+	@Autowired
+	private ICategoryService categoryService;
+
+	@GetMapping
+	public ResponseEntity<ResponseModel<List<CategoryDto>>> listarCategorias() {
+		ResponseModel<List<CategoryDto>> response = new ResponseModel<>();
+		response.setData(categoryService.listar());
+		response.setStatusCode(HttpStatus.OK.value());
+		response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).listarCategorias())
+				.withSelfRel());
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ResponseModel<CategoryDto>> consultarCategorias(@PathVariable Long id) {
+		ResponseModel<CategoryDto> response = new ResponseModel<>();
+		response.setData(categoryService.consultar(id));
+		response.setStatusCode(HttpStatus.OK.value());
+		response.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).consultarCategorias(id)).withSelfRel());
+		response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).deletarCategoria(id))
+				.withRel("DELETE"));
+		response.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).atualizarCategoria(response.getData()))
+				.withRel("UPDATE"));
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@PostMapping
+	public ResponseEntity<ResponseModel<Boolean>> cadastrarCategoria(@Valid @RequestBody CategoryDto category) {
+		ResponseModel<Boolean> response = new ResponseModel<>();
+		response.setData(categoryService.cadastrar(category));
+		response.setStatusCode(HttpStatus.CREATED.value());
+		response.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).cadastrarCategoria(category))
+				.withSelfRel());
+		response.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).atualizarCategoria(category))
+				.withRel("UPDATE"));
+		response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).listarCategorias())
+				.withRel("GET_ALL"));
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@PutMapping
+	public ResponseEntity<ResponseModel<Boolean>> atualizarCategoria(@Valid @RequestBody CategoryDto category) {
+		ResponseModel<Boolean> response = new ResponseModel<>();
+		response.setData(categoryService.atualizar(category));
+		response.setStatusCode(HttpStatus.OK.value());
+		response.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).atualizarCategoria(category))
+				.withSelfRel());
+		response.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).deletarCategoria(category.getId()))
+				.withRel("DELETE"));
+		response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).listarCategorias())
+				.withRel("GET_ALL"));
+		response.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).consultarCategorias(category.getId()))
+				.withRel("GET"));
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ResponseModel<Boolean>> deletarCategoria(@PathVariable Long id) {
+		ResponseModel<Boolean> response = new ResponseModel<>();
+		response.setData(categoryService.excluir(id));
+		response.setStatusCode(HttpStatus.OK.value());
+		response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).deletarCategoria(id))
+				.withSelfRel());
+		response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).listarCategorias())
+				.withRel("GET_ALL"));
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
 }
--- src/main/java/com/learning/financescontroll/v1/service/CategoryService.java
+++ src/main/java/com/learning/financescontroll/v1/service/CategoryService.java
@@ -8,23 +8,25 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.learning.financescontroll.entity.CategoryEntity;
 import com.learning.financescontroll.repository.ICategoryRepository;
+import com.learning.financescontroll.v1.controller.CategoryController;
 import com.learning.financescontroll.v1.dto.CategoryDto;
 import com.learning.financescontroll.v1.exception.CategoryException;
 
 @CacheConfig(cacheNames = "categoria")
 @Service
 public class CategoryService implements ICategoryService {
-	
+
 	private String ERRO_GENERICO = "Erro interno identificado, Contate o suporte";
-	
+
 	private ICategoryRepository categoryRepository;
 	private ModelMapper mapper;
-	
+
 	@Autowired
 	public CategoryService(ICategoryRepository categoryRepository) {
 		this.mapper = new ModelMapper();
@@ -35,7 +37,15 @@ public class CategoryService implements ICategoryService {
 	@Override
 	public List<CategoryDto> listar() {
 		try {
-			return this.mapper.map(this.categoryRepository.findAll(), new TypeToken<List<CategoryDto>>() {}.getType());
+			List<CategoryDto> categoryDto = this.mapper.map(this.categoryRepository.findAll(),
+					new TypeToken<List<CategoryDto>>() {
+					}.getType());
+
+			categoryDto.forEach(category -> category.add(WebMvcLinkBuilder
+					.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).consultarCategorias(category.getId()))
+					.withSelfRel()));
+
+			return categoryDto;
 		} catch (CategoryException c) {
 			throw c;
 		} catch (Exception e) {
@@ -65,7 +75,7 @@ public class CategoryService implements ICategoryService {
 			if (category.getId() != null) {
 				throw new CategoryException("Não pode existir ID em cadastrar.", HttpStatus.BAD_REQUEST);
 			}
-			
+
 			CategoryEntity categoryEntity = this.mapper.map(category, CategoryEntity.class);
 			this.categoryRepository.save(categoryEntity);
 			return Boolean.TRUE;
@@ -82,9 +92,9 @@ public class CategoryService implements ICategoryService {
 			if (category.getId() == null) {
 				return Boolean.FALSE;
 			}
-			
+
 			this.consultar(category.getId());
-			
+
 			CategoryEntity categoryEntity = this.mapper.map(category, CategoryEntity.class);
 			this.categoryRepository.save(categoryEntity);
 			return Boolean.TRUE;
